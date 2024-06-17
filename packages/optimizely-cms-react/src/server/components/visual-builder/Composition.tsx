c25ae296bcda5747657d1216433bbe7e0ec23a52
Remko Jantzen <693172+remkoj@users.noreply.github.com>
2024-06-17 17:32:11 +0200

    Hardened logic to throw a nice Error instead of an unhandled exception

--- packages/optimizely-cms-react/src/server/components/visual-builder/Composition.tsx
+++ packages/optimizely-cms-react/src/server/components/visual-builder/Composition.tsx
@@ -1,65 +1,8 @@
 import 'server-only'
-import { createElement, type FunctionComponent } from 'react'
-import { isElementNode } from './functions.js'
+import { isElementNode, defaultNodePropsFactory, defaultPropsFactory } from './functions.js'
 import { CmsContent } from '../cms-content.js'
-import type { ContentType } from '../../../types.js'
-import { isContentLink, ContentLinkWithLocale, isInlineContentLink } from '@remkoj/optimizely-graph-client'
-import type { OptimizelyCompositionProps, LeafPropsFactory, CompositionElementNode, NodePropsFactory, CompositionStructureNode } from './types.js'
+import type { OptimizelyCompositionProps } from './types.js'
 import getServerContext from '../../context.js'
-
-function isContentType(toTest: any) : toTest is ContentType
-{
-    return Array.isArray(toTest) && toTest.every(x => typeof(x) == 'string' && x.length > 0)
-}
-
-const defaultPropsFactory : LeafPropsFactory = <ET extends Record<string, any>, LT = string>(node: CompositionElementNode<ET>) => {
-    const contentType = node.element?._metadata?.types
-    if (!isContentType(contentType))
-        throw new Error("Invalid content type: "+JSON.stringify(contentType))
-
-    const contentLink : Partial<ContentLinkWithLocale<LT>> = {
-        key: node.element?._metadata?.key || node.key || undefined,
-        version: node.element?._metadata?.version,
-        locale: node.element?._metadata?.locale
-    }
-    if (!(isContentLink(contentLink) || isInlineContentLink(contentLink)))
-        throw new Error("Invalid content link: "+JSON.stringify(contentLink))
-
-    const layoutData = {
-        type: node.type,
-        layoutType: node.layoutType,
-        template: node.template,
-        settings: node.settings
-    }
-
-    return [ contentLink, contentType, node.element, layoutData ]
-}
-
-function ucFirst(input: string): string
-{
-    return input[0].toUpperCase() + input.substring(1)
-}
-
-const defaultNodePropsFactory : NodePropsFactory = <ET extends Record<string, any>, LT = string>(node: CompositionStructureNode) => {
-    const componentTypes = [
-        [ node.template, node.type ? ucFirst(node.type) : null, ucFirst(node.layoutType), "Component", "Content"].filter(x => x) as string[],
-        (node.template && node.type) ? [ node.type ? ucFirst(node.type) : null, ucFirst(node.layoutType), "Component", "Content"].filter(x => x) as string[] : null,
-        ["Node","Component","Content"]
-    ].filter(x => x) as Array<Array<string>>
-    const contentLink : ContentLinkWithLocale<LT> = { key: node.key ?? '' }
-    const componentData : ET = {} as ET
-    const layoutData = {
-        type: node.type,
-        layoutType: node.layoutType,
-        template: node.template,
-        settings: node.settings
-    }
-
-    if (!(isContentLink(contentLink) || isInlineContentLink(contentLink)))
-        throw new Error("Invalid content link: "+JSON.stringify(contentLink))
-
-    return [ contentLink, componentTypes, componentData, layoutData ]
-}
 
 /**
  * Render the composition as made available through Optimizely Graph for Visual Builder
@@ -69,13 +12,18 @@ const defaultNodePropsFactory : NodePropsFactory = <ET extends Record<string, an
  */
 export async function OptimizelyComposition({ node, leafPropsFactory = defaultPropsFactory, nodePropsFactory = defaultNodePropsFactory}: OptimizelyCompositionProps) : Promise<JSX.Element>
 {
+    const { factory, isDebug } = getServerContext()
+
     if (isElementNode(node)) {
+        if (isDebug)
+            console.log(`⚪ [VisualBuilder] Rendering element node ${ JSON.stringify(node) }`)
         const [ contentLink, contentType, fragmentData, layoutProps ] = leafPropsFactory(node)
         //@ts-expect-error CmsContent is an Asynchronous server component, which isn't supported by the generic React Typings
         return <CmsContent contentLink={ contentLink } contentType={ contentType } fragmentData={ fragmentData } layoutProps={ layoutProps } />
     }
+    if (isDebug)
+        console.log(`⚪ [VisualBuilder] Rendering structure node ${ JSON.stringify(node) }`)
 
-    const { factory, isDebug } = getServerContext()
     if (!factory)
         throw new Error("🟡 [VisualBuilder] [OptimizelyComposition] The factory must be defined within the serverContext")
 
@@ -95,8 +43,6 @@ export async function OptimizelyComposition({ node, leafPropsFactory = defaultPr
     return <CmsContent contentType={contentType} contentLink={contentLink} fragmentData={fragmentData} layoutProps={layoutProps} >
         {(node.nodes ?? []).map((child) => {
             const childKey = child.key ? child.key : `vb::${ JSON.stringify( child )}`
-            if (isDebug)
-                console.log(`⚪ [VisualBuilder] Generated child key: ${ childKey }`)
             //@ts-expect-error OptimizelyComposition is an Asynchronous server component, which isn't supported by the generic React Typings
             return <OptimizelyComposition key={ childKey } node={ child } leafPropsFactory={ leafPropsFactory } nodePropsFactory={ nodePropsFactory } />
         })}
--- packages/optimizely-cms-react/src/server/components/visual-builder/functions.ts
+++ packages/optimizely-cms-react/src/server/components/visual-builder/functions.ts
@@ -1,4 +1,6 @@
-import { CompositionNode, CompositionElementNode, CompositionStructureNode } from './types.js'
+import type { ContentType } from '../../../types.js'
+import { isContentLink, ContentLinkWithLocale, isInlineContentLink } from '@remkoj/optimizely-graph-client'
+import type { CompositionNode, LeafPropsFactory, CompositionElementNode, NodePropsFactory, CompositionStructureNode } from './types.js'
 
 export function isElementNode(node: CompositionNode<Record<string,any>>) : node is CompositionElementNode<Record<string,any>>
 {
@@ -27,4 +29,60 @@ export function isNode(toTest: any) : toTest is CompositionNode
     const hasValidType = typeof (toTest as CompositionNode).layoutType == 'string' && nodeTypes.includes((toTest as CompositionNode).layoutType)
 
     return hasValidName && hasValidType
+}
+
+export function isContentType(toTest: any) : toTest is ContentType
+{
+    return Array.isArray(toTest) && toTest.every(x => typeof(x) == 'string' && x.length > 0)
+}
+
+export const defaultPropsFactory : LeafPropsFactory = <ET extends Record<string, any>, LT = string>(node: CompositionElementNode<ET>) => {
+    const contentType = node.element?._metadata?.types
+    if (!isContentType(contentType))
+        throw new Error("Invalid content type: "+JSON.stringify(contentType))
+
+    const contentLink : Partial<ContentLinkWithLocale<LT>> = {
+        key: node.element?._metadata?.key || node.key || undefined,
+        version: node.element?._metadata?.version,
+        locale: node.element?._metadata?.locale
+    }
+    if (!(isContentLink(contentLink) || isInlineContentLink(contentLink)))
+        throw new Error("Invalid content link: "+JSON.stringify(contentLink))
+
+    const layoutData = {
+        type: node.type,
+        layoutType: node.layoutType,
+        template: node.template,
+        settings: node.settings
+    }
+
+    return [ contentLink, contentType, node.element, layoutData ]
+}
+
+export const defaultNodePropsFactory : NodePropsFactory = <ET extends Record<string, any>, LT = string>(node: CompositionStructureNode) => {
+    const componentTypes = [
+        [ node.template, ucFirst(node.type), ucFirst(node.layoutType), "Component", "Content"].filter(x => x) as string[],
+        (node.template && node.type) ? [ node.type ? ucFirst(node.type) : null, ucFirst(node.layoutType), "Component", "Content"].filter(x => x) as string[] : null,
+        ["Node","Component","Content"]
+    ].filter(x => x) as Array<Array<string>>
+    const contentLink : ContentLinkWithLocale<LT> = { key: node.key ?? '' }
+    const componentData : ET = {} as ET
+    const layoutData = {
+        type: node.type,
+        layoutType: node.layoutType,
+        template: node.template,
+        settings: node.settings
+    }
+
+    if (!(isContentLink(contentLink) || isInlineContentLink(contentLink)))
+        throw new Error("🔴 [VisualBuilder] Invalid content link: "+JSON.stringify(contentLink) +" - Node: "+ JSON.stringify(node))
+
+    return [ contentLink, componentTypes, componentData, layoutData ]
+}
+
+export function ucFirst(input: string | undefined | null): string | null
+{
+    if (typeof(input) == 'string' && input.length > 0)
+        return input[0].toUpperCase() + input.substring(1)
+    return null
 }
