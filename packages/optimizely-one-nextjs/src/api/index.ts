edeb2b8fd145c364a9723de1ebe0a1068151e2d0
Remko Jantzen <693172+remkoj@users.noreply.github.com>
2025-05-28 14:50:38 +0200

    Small Next.js 15 fixes

--- packages/optimizely-one-nextjs/src/api/index.ts
+++ packages/optimizely-one-nextjs/src/api/index.ts
@@ -8,69 +8,67 @@ import ExperimentationApiService from './experimentation-api'
 import ContentRecsApiService from './content-recs'
 import ContentRecsGoalsService from './content-goals'
 
-type RequestContext = { params: Record<string, string | string[]> } 
+type RequestContext = { params: Promise<Record<string, string | string[]>> }
 type OptimizelyOneApi = (req: NextRequest, ctx: RequestContext) => Promise<NextResponse>
 
 export type OptimizelyOneApiConfig = {
-    /**
-     * The URL Parameter you've setup in your Next.JS router to catch the 
-     * full path to be handled.
-     */
-    pathParameterName: string
+  /**
+   * The URL Parameter you've setup in your Next.JS router to catch the 
+   * full path to be handled.
+   */
+  pathParameterName: string
 
-    /**
-     * Inject any project specific services into the OptimizelyOne API
-     */
-    services: ApiService[]
+  /**
+   * Inject any project specific services into the OptimizelyOne API
+   */
+  services: ApiService[]
 }
 
-export function createOptimizelyOneApi(config?: Partial<OptimizelyOneApiConfig>) : OptimizelyOneApi
-{
-    const pathParameterName = config?.pathParameterName ?? 'path'
-    const services = [
-        ...(config?.services || []),
-        ProfileApiService,
-        GraphInfoApiService,
-        ExperimentationApiService,
-        ContentRecsApiService,
-        ContentRecsGoalsService
-    ]
+export function createOptimizelyOneApi(config?: Partial<OptimizelyOneApiConfig>): OptimizelyOneApi {
+  const pathParameterName = config?.pathParameterName ?? 'path'
+  const services = [
+    ...(config?.services || []),
+    ProfileApiService,
+    GraphInfoApiService,
+    ExperimentationApiService,
+    ContentRecsApiService,
+    ContentRecsGoalsService
+  ]
 
-    // Basic service matcher, but get's the job done for now
-    function matchService(verb: string, path: string, serviceFor: ApiService['for']) : boolean
-    {
-        return serviceFor.path.toLowerCase() == path.toLowerCase() && 
-            serviceFor.verb.toLowerCase() == verb.toLowerCase()
-    }
+  // Basic service matcher, but get's the job done for now
+  function matchService(verb: string, path: string, serviceFor: ApiService['for']): boolean {
+    return serviceFor.path.toLowerCase() == path.toLowerCase() &&
+      serviceFor.verb.toLowerCase() == verb.toLowerCase()
+  }
 
-    async function handler(request: NextRequest, context: RequestContext) : Promise<NextResponse>
-    {
-        // Determine the current path & basePath for handling the request
-        let apiPath : string | string[] | undefined = context.params[pathParameterName]
-        if (!apiPath) apiPath = '/'
-        else if (Array.isArray(apiPath)) apiPath = '/' + apiPath.join('/')
-        else if (!apiPath.startsWith('/')) apiPath = '/' + apiPath
-        const path : string = apiPath
-        //const basePath = apiPath == '/' ? request.nextUrl.pathname : request.nextUrl.pathname.endsWith(apiPath) ? request.nextUrl.pathname.substring(0, request.nextUrl.pathname.lastIndexOf(apiPath)) : ''
-        const verb = request.method
+  async function handler(request: NextRequest, { params: asyncParams }: RequestContext): Promise<NextResponse> {
+    // Determine the current path & basePath for handling the request
+    const params = await asyncParams
+    let apiPath: string | string[] | undefined = params[pathParameterName]
+    if (!apiPath) apiPath = '/'
+    else if (Array.isArray(apiPath)) apiPath = '/' + apiPath.join('/')
+    else if (!apiPath.startsWith('/')) apiPath = '/' + apiPath
+    const path: string = apiPath
+    //const basePath = apiPath == '/' ? request.nextUrl.pathname : request.nextUrl.pathname.endsWith(apiPath) ? request.nextUrl.pathname.substring(0, request.nextUrl.pathname.lastIndexOf(apiPath)) : ''
+    const verb = request.method
 
-        // Find the service
-        const apiService = services.find(service => matchService(verb,path,service.for))
-        if (!apiService)
-            return NextResponse.json({ error: { status: 404, message: "Not found" }}, { status: 404 })
+    // Find the service
+    const apiService = services.find(service => matchService(verb, path, service.for))
+    if (!apiService)
+      return NextResponse.json({ error: { status: 404, message: "Not found" } }, { status: 404 })
 
-        // Invoke the service
-        const c = cookies()
-        const [ response, statusCode, contentType ] = await apiService.handler(request.nextUrl.searchParams, c)
+    // Invoke the service
+    const c = cookies()
+    const [response, statusCode, contentType] = await apiService.handler(request.nextUrl.searchParams, c)
 
-        const headers = new Headers();
-        if (contentType)
-            headers.set("Content-Type", contentType)
+    const headers = new Headers();
+    if (contentType)
+      headers.set("Content-Type", contentType)
 
-        // Return the outcome
-        if (typeof response == 'string')
-            return new NextResponse(response, { headers, status: statusCode })
-        return NextResponse.json(response, { headers, status: statusCode })
-    }
-    return handler
+    // Return the outcome
+    if (typeof response == 'string')
+      return new NextResponse(response, { headers, status: statusCode })
+    return NextResponse.json(response, { headers, status: statusCode })
+  }
+  return handler
 }
--- packages/optimizely-one-nextjs/src/components/optimizely-one-gadget/index.tsx
+++ packages/optimizely-one-nextjs/src/components/optimizely-one-gadget/index.tsx
@@ -4,28 +4,37 @@ import dynamic from 'next/dynamic'
 import { Suspense } from 'react'
 import 'server-only'
 
-const OptimizelyOneClientGadget = dynamic(() => import('./gadget'), { ssr: false })
+const OptimizelyOneClientGadget = dynamic(() => import('./gadget'))
 
 export const OptimizelyOneGadget = ({
-    refreshInterval = 0,
-    servicePrefix = '/api/me',
-    show
-}: OptimizelyOneGadgetProps) =>
-{
-    const optiOneConfig = getConfig()
-    const { HelperEnabled, OptimizelyDebug } = optiOneConfig
-    const productStatus = checkProductStatus(optiOneConfig)
-    
-    if (show || (show === undefined && HelperEnabled))
-    {
-        return <Suspense fallback={null}>
-            <OptimizelyOneClientGadget refreshInterval={refreshInterval} servicePrefix={servicePrefix} show={show} showContentRecs={ productStatus.contentRecsApi } showDataPlatform={ productStatus.dataPlatform } showWebEx={ productStatus.webExperimentation } />
-        </Suspense>
-    }
+  refreshInterval = 0,
+  servicePrefix = '/api/me',
+  show,
+}: OptimizelyOneGadgetProps) => {
+  const optiOneConfig = getConfig()
+  const { HelperEnabled, OptimizelyDebug } = optiOneConfig
+  const productStatus = checkProductStatus(optiOneConfig)
 
-    if (OptimizelyDebug)
-        console.log(`🚀 [OptimizelyOne Gadget] Gadget disabled by ${ show === undefined ? 'configuration' : 'parameter'}`)
-    return null
+  if (show || (show === undefined && HelperEnabled)) {
+    return (
+      <Suspense fallback={null}>
+        <OptimizelyOneClientGadget
+          refreshInterval={refreshInterval}
+          servicePrefix={servicePrefix}
+          show={show}
+          showContentRecs={productStatus.contentRecsApi}
+          showDataPlatform={productStatus.dataPlatform}
+          showWebEx={productStatus.webExperimentation}
+        />
+      </Suspense>
+    )
+  }
+
+  if (OptimizelyDebug)
+    console.log(
+      `🚀 [OptimizelyOne Gadget] Gadget disabled by ${show === undefined ? 'configuration' : 'parameter'}`
+    )
+  return null
 }
 
 export default OptimizelyOneGadget
