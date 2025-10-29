65e217037fb07487df3b32b5c2bf80fec8d75a32
Remko Jantzen <693172+remkoj@users.noreply.github.com>
2025-10-29 14:27:15 +0100

    Quick fix on GraphQL Codegen preset

--- packages/optimizely-graph-functions/src/preset.ts
+++ packages/optimizely-graph-functions/src/preset.ts
@@ -17,38 +17,49 @@ import {
   injectPageQueries,
   performInjections,
   cleanFragmentSpreads,
-  handleDependDirective
+  handleDependDirective,
 } from './transform'
 import type { PresetOptions } from './types'
 
 export type { PresetOptions } from './types'
 
-export const preset: Types.OutputPreset<PresetOptions> =
-{
+export const preset: Types.OutputPreset<PresetOptions> = {
   /**
    * Prepare the documents to be parsed by this preset, without modifying the original array
-   * 
+   *
    * @param       outputFilePath              The path where the output of this preset will be stored
    * @param       outputSpecificDocuments     The currently selected documents
    * @returns     An awaitable with the modified list of documents.
    */
-  prepareDocuments: async (outputFilePath: Readonly<string>, outputSpecificDocuments: ReadonlyArray<Types.OperationDocument>) => {
+  prepareDocuments: async (
+    outputFilePath: Readonly<string>,
+    outputSpecificDocuments: ReadonlyArray<Types.OperationDocument>
+  ) => {
     // Get the configured documents
-    const optiDocs = outputSpecificDocuments.filter<string>((x => typeof (x) == 'string' && x.startsWith('opti-cms:')) as (x: Types.OperationDocument) => x is string)
-    const normalDocs = outputSpecificDocuments.filter(x => !(typeof (x) == 'string' && x.startsWith('opti-cms:')))
+    const optiDocs = outputSpecificDocuments.filter<string>(
+      ((x) => typeof x == 'string' && x.startsWith('opti-cms:')) as (
+        x: Types.OperationDocument
+      ) => x is string
+    )
+    const normalDocs = outputSpecificDocuments.filter(
+      (x) => !(typeof x == 'string' && x.startsWith('opti-cms:'))
+    )
 
     // Get the base documents
-    const documents = clientPreset.prepareDocuments ?
-      await clientPreset.prepareDocuments(outputFilePath, normalDocs) :
-      [...normalDocs, `!${outputFilePath}`]
+    const documents = clientPreset.prepareDocuments
+      ? await clientPreset.prepareDocuments(outputFilePath, normalDocs)
+      : [...normalDocs, `!${outputFilePath}`]
 
     // Transform / inject the Opti-CMS documents
-    const CmsDocLoaders: Array<Types.CustomDocumentLoader> =
-      (optiDocs.length == 0 ? ['opti-cms:/fragments/13', 'opti-cms:/queries/13'] : optiDocs).map(optiDoc => {
-        const loader: Types.CustomDocumentLoader = {}
-        loader[optiDoc] = { loader: "@remkoj/optimizely-graph-functions/loader" }
-        return loader
-      })
+    const CmsDocLoaders: Array<Types.CustomDocumentLoader> = (
+      optiDocs.length == 0
+        ? ['opti-cms:/fragments/13', 'opti-cms:/queries/13']
+        : optiDocs
+    ).map((optiDoc) => {
+      const loader: Types.CustomDocumentLoader = {}
+      loader[optiDoc] = { loader: '@remkoj/optimizely-graph-functions/loader' }
+      return loader
+    })
 
     // Create a new, extended, array
     return [...documents, ...CmsDocLoaders]
@@ -65,10 +76,10 @@ export const preset: Types.OutputPreset<PresetOptions> =
       ...options.config,
 
       // Enforced settings
-      namingConvention: "keep", // Keep casing "as-is" from Optimizely Graph
+      namingConvention: 'keep', // Keep casing "as-is" from Optimizely Graph
     }
 
-    // Change the default for fragment masking from 'useFragment' to 
+    // Change the default for fragment masking from 'useFragment' to
     // 'getFragmentData', in order to prevent issues with code checks for
     // React hooks
     if (options.presetConfig.fragmentMasking !== false) {
@@ -76,31 +87,52 @@ export const preset: Types.OutputPreset<PresetOptions> =
         ...options.presetConfig,
         fragmentMasking: {
           unmaskFunctionName: 'getFragmentData',
-          ...(typeof (options.presetConfig?.fragmentMasking) == 'object' ? options.presetConfig?.fragmentMasking : {})
-        }
+          ...(typeof options.presetConfig?.fragmentMasking == 'object'
+            ? options.presetConfig?.fragmentMasking
+            : {}),
+        },
       }
     }
 
+    function injectOptimizelyTransforms(
+      currentTransforms?: Types.ConfiguredDocumentTransform<object>[]
+    ): Types.ConfiguredDocumentTransform<object>[] {
+      const dt = currentTransforms ?? []
+      dt.unshift({
+        name: 'Optimizely.CMS',
+        transformObject: {
+          transform: (tOpts) =>
+            executeDocumentTransforms(
+              tOpts.documents,
+              [
+                cleanFragments, // Remove fragments that target non-existing types
+                normalizeFragmentNames, // Allow overriding of built-in fragments
+                normalizeQueryNames, // Allow overriding of built-in queries
+                injectComponentDocuments, // Inject fragments to fetch component data
+                injectPageQueries, // Inject queries to fetch page/experience data
+                injectSectionQueries, // Inject queries to fetch section data
+                performInjections, // Run injections of component fragments adjacent to placeholder fragments
+                cleanFragmentSpreads, // Remove all fragment spreads that target a fragment that does not exist in the documents
+                handleDependDirective, // Remove the "item" field in queries and fragments from the "ContentReference" type if it's not in the schema
+              ],
+              options
+            ),
+        },
+      })
+      return dt
+    }
+    function hasOptimizelyTransform(currentTransforms?: Types.ConfiguredDocumentTransform<object>[])
+    {
+      if (!Array.isArray(currentTransforms)) return false
+      return currentTransforms.some(x => x.name === 'Optimizely.CMS')
+    }
+
     // Apply all changes to the document set, prior to validating it. They're executed in the order of the array
-    const dt = options.documentTransforms || []
-    dt.unshift({
-      name: "Optimizely CMS Document Transforms",
-      transformObject: {
-        transform: (tOpts) => executeDocumentTransforms(tOpts.documents, [
-          cleanFragments,           // Remove fragments that target non-existing types
-          normalizeFragmentNames,   // Allow overriding of built-in fragments
-          normalizeQueryNames,      // Allow overriding of built-in queries
-          injectComponentDocuments, // Inject fragments to fetch component data
-          injectPageQueries,        // Inject queries to fetch page/experience data
-          injectSectionQueries,     // Inject queries to fetch section data
-          performInjections,        // Run injections of component fragments adjacent to placeholder fragments
-          cleanFragmentSpreads,     // Remove all fragment spreads that target a fragment that does not exist in the documents
-          handleDependDirective     // Remove the "item" field in queries and fragments from the "ContentReference" type if it's not in the schema
-        ], options)
-      }
-    })
-    options.documentTransforms = dt;
-    /*options.documents = await executeDocumentTransforms(options.documents, [
+    options.documentTransforms = injectOptimizelyTransforms(
+      options.documentTransforms
+    )
+
+    options.documents = await executeDocumentTransforms(options.documents, [
       cleanFragments,           // Remove fragments that target non-existing types
       normalizeFragmentNames,   // Allow overriding of built-in fragments
       normalizeQueryNames,      // Allow overriding of built-in queries
@@ -110,31 +142,32 @@ export const preset: Types.OutputPreset<PresetOptions> =
       performInjections,        // Run injections of component fragments adjacent to placeholder fragments
       cleanFragmentSpreads,     // Remove all fragment spreads that target a fragment that does not exist in the documents
       handleDependDirective     // Remove the "item" field in queries and fragments from the "ContentReference" type if it's not in the schema
-    ], options);*/
+    ], options);
 
     // Build the preset files
-    const section: Array<Types.GenerateOptions> = await clientPreset.buildGeneratesSection(options)
-    
-    // Add GraphQL Request Client. 
+    const section: Array<Types.GenerateOptions> =
+      await clientPreset.buildGeneratesSection(options)
+
+    // Add GraphQL Request Client.
     section.push({
       filename: `${options.baseOutputDir}client.ts`,
       pluginMap: {
-        "add": AddPlugin,
-        "typescript-graphql-request": GraphQLRequestPlugin
+        add: AddPlugin,
+        'typescript-graphql-request': GraphQLRequestPlugin,
       },
       plugins: [
         {
           add: {
-            content: ["import type * as Schema from \"./graphql\";"]
+            content: ['import type * as Schema from "./graphql";'],
           },
         },
         {
           'typescript-graphql-request': {
             ...options.config,
             useTypeImports: true,
-            importOperationTypesFrom: "Schema",
-          }
-        }
+            importOperationTypesFrom: 'Schema',
+          },
+        },
       ],
       schema: options.schema,
       schemaAst: options.schemaAst,
@@ -143,20 +176,20 @@ export const preset: Types.OutputPreset<PresetOptions> =
       },
       profiler: options.profiler,
       documents: options.documents,
-      documentTransforms: options.documentTransforms
+      documentTransforms: options.documentTransforms,
     })
 
-    // Add the functions file, which will materialize the defined 
-    // functions. 
+    // Add the functions file, which will materialize the defined
+    // functions.
     section.push({
       filename: `${options.baseOutputDir}functions.ts`,
       pluginMap: {
-        ['optly-functions']: plugin
+        ['optly-functions']: plugin,
       },
       plugins: [
         {
-          ['optly-functions']: {}
-        }
+          ['optly-functions']: {},
+        },
       ],
       schema: options.schema,
       schemaAst: options.schemaAst,
@@ -166,39 +199,50 @@ export const preset: Types.OutputPreset<PresetOptions> =
         ...pickPluginOptions(options.presetConfig),
       },
       documents: options.documents,
-      documentTransforms: options.documentTransforms
+      documentTransforms: options.documentTransforms,
     })
 
     // Update file generation configs
     section.forEach((fileConfig, idx) => {
-
       // Modify index.ts with additional exports
-      if (fileConfig.filename.endsWith("index.ts")) {
+      if (fileConfig.filename.endsWith('index.ts')) {
         section[idx].plugins.unshift({
           add: {
             content: [
               'export * as Schema from "./graphql";',
               'export * from "./functions";',
               'export { getSdk, type Sdk } from "./client";',
-            ]
-          }
+            ],
+          },
         })
         section[idx].plugins.push({
           add: {
-            content: ['', `export const WITH_RECURSIVE_SUPPORT = ${options.presetConfig.recursion === true ? 'true' : 'false'};`]
-          }
+            content: [
+              '',
+              `export const WITH_RECURSIVE_SUPPORT = ${options.presetConfig.recursion === true ? 'true' : 'false'};`,
+            ],
+          },
         })
       }
+
+      if (!hasOptimizelyTransform(fileConfig.documentTransforms))
+        fileConfig.documentTransforms = injectOptimizelyTransforms(fileConfig.documentTransforms)
 
       // Optimizely Graph supports recursive queries to allow fetching
       // data as created in the CMS. This can cause issues when using
       // multiple GraphQL sources, hence the ability to enable/disalbe
       // the support for recursive queries.
-      if (fileConfig.skipDocumentsValidation != true && options.presetConfig.recursion === true) {
+      if (
+        fileConfig.skipDocumentsValidation != true &&
+        options.presetConfig.recursion === true
+      ) {
         const currentOptions = fileConfig.skipDocumentsValidation || {}
         section[idx].skipDocumentsValidation = {
           ...currentOptions,
-          ignoreRules: [...(currentOptions.ignoreRules ?? []), 'NoFragmentCyclesRule']
+          ignoreRules: [
+            ...(currentOptions.ignoreRules ?? []),
+            'NoFragmentCyclesRule',
+          ],
         }
       }
     })
