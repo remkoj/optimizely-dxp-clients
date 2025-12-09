826a8b658a4a2daa7f83ee0cfa882be4ee29b717
Remko Jantzen <693172+remkoj@users.noreply.github.com>
2025-12-09 10:05:02 +0100

    Initial fix

--- packages/optimizely-one-nextjs/src/api/content-recs.ts
+++ packages/optimizely-one-nextjs/src/api/content-recs.ts
@@ -7,13 +7,17 @@ import * as Responses from './responses'
 export const ContentRecsApiService: ApiService<any> = {
   for: { path: '/crecs', verb: 'get' },
   handler: async (query, cookies) => {
+    // Read configuration
     const optiOneConfig = readConfigFromEnv();
     if (!optiOneConfig.ContentRecsHost)
       return Responses.NotFound
 
+    // Identify visitor
     const crId = ContentRecs.Tools.getVisitorID(cookies)
     if (!crId)
       return [{ total_hits: 0, content: [] }, 200]
+
+    // Send request to Content Recommendations, and strip JSONP function call
     const recsService = new URL(`https://api.${optiOneConfig.ContentRecsHost}/1.0/users/idio_visitor_id:${crId}/content?callback=fn`)
     query.forEach((value, name) => {
       if (name.toLowerCase() != 'callback')
@@ -24,16 +28,15 @@ export const ContentRecsApiService: ApiService<any> = {
       return [{ error: { status: snippetResponse.status, message: snippetResponse.statusText } }, snippetResponse.status]
     const body = await snippetResponse.text()
     const jsonData = body.substring(body.indexOf('{'), body.lastIndexOf('}') + 1)
+
+    // Convert and return data
     try {
       const recsData = JSON.parse(jsonData)
-      return [
-        {
-          ...recsData,
-          next_page: (new URL(recsData.next_page)).search
-        },
-        200
-      ]
-    } catch {
+      if (recsData.next_page)
+        recsData.next_page = (new URL(recsData.next_page)).search
+      return [ recsData, 200 ]
+    } catch (e) {
+      console.error("Error in processing Content Recommendations", e)
       return [{ error: { status: 500, message: "Unable to parse Content Recommendations data" } }, 500]
     }
   }
