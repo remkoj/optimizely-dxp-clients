ce7ad2b89afd760136eb4aad27545f42549507d5
Nadia Mayangputri <nadia@luminary.com>
2026-06-01 14:28:03 +0800

    Update getContentByPath to check both simple address path and hierarchical path

--- packages/optimizely-graph-functions/src/documents/queries.cms13.ts
+++ packages/optimizely-graph-functions/src/documents/queries.cms13.ts
@@ -27,10 +27,10 @@ export default [
   `query getContentByPath($path: [String!]!, $locale: [Locales!], $siteId: String, $changeset: String = null) {
       content: _Content(
         where: {
-          _metadata: {
-            url: { default: { in: $path }, base: { eq: $siteId } }
-            changeset: { eq: $changeset }
-          }
+          _or: [
+              { _metadata: { url: { default: { in: $path }, base: { eq: $siteId } }, changeset: { eq: $changeset } } }
+              { _metadata: { url: { hierarchical: { in: $path }, type: { eq: "SIMPLE" }, base: { eq: $siteId } }, changeset: { eq: $changeset } } }
+          ]
         }
         locale: $locale
       ) {
